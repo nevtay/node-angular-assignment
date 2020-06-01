0cb703d999e2ca9829b815094b49149ba5d43886
nevtay <nevintay@gmail.com>
2020-06-02 00:54:28 +0800

    Import 'add-report' component to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AddReportComponent } from './components/add-report/add-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddReportComponent
   ],
   imports: [
     BrowserModule,
