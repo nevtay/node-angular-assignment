edbb7a59ba512f23a6cd3f4c8096434919cfe150
nevtay <nevintay@gmail.com>
2020-06-01 22:38:11 +0800

    Instantiate variables to store input values from username and password, and the appropriate functions to handle them

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username:string = '';
+
+  password:string = '';
+
+  updateUsername() {
+    
+  }
+
+  updatePassword() {
+    
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
