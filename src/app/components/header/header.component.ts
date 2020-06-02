f6f63e1e0931701257a3488854550ba3f2fafdd6
nevtay <nevintay@gmail.com>
2020-06-02 18:30:50 +0800

    Add routing config to ngOnInit()

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() { }
 
 }
