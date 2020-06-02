def4efa49b885959a5bd5bac8fd3bcb4bf357f0d
nevtay <nevintay@gmail.com>
2020-06-03 00:44:34 +0800

    Initial commit; reports are mocks

---
+++ src/app/models/Reports.ts
@@ -0,0 +1,12 @@
+import { Report } from "./Report"
+
+export const REPORTS: Report[] = [
+    { date: '13 May 2020', username: 'User 1', content: 'sample content', isApproved: false },
+    { date: '14 May 2020', username: 'User 2', content: 'sample content2', isApproved: false },
+    { date: '15 May 2020', username: 'User 3', content: 'sample content3', isApproved: false },
+    { date: '16 May 2020', username: 'User 4', content: 'sample content4', isApproved: false },
+    { date: '17 May 2020', username: 'User 5', content: 'sample content5', isApproved: false },
+    { date: '18 May 2020', username: 'User 6', content: 'sample content6', isApproved: false },
+    { date: '19 May 2020', username: 'User 7', content: 'sample content7', isApproved: false },
+    { date: '20 May 2020', username: 'User 8', content: 'sample content8', isApproved: false },
+]
