918637dfd782e137b781bbf014456b02fc8bb4c3
nevtay <nevintay@gmail.com>
2020-06-02 18:24:10 +0800

    Create routes and map them their respective components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AddReportComponent } from './components/add-report/add-report.component';
+import { ViewReportsComponent } from './components/view-reports/view-reports.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create-report', component: AddReportComponent},
+  {path: 'view-reports', component: ViewReportsComponent},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
