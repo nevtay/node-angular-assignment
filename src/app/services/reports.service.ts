f6842a214593b5444569e27a8ca5235167988959
nevtay <nevintay@gmail.com>
2020-06-03 00:07:09 +0800

    Initial commit

---
+++ src/app/services/reports.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Report } from '../models/Report'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ReportService {
+
+  constructor() { }
+
+}
