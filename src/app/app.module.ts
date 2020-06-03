1d7a3bbb98e8c91d4275d36054851ab22b453e4d
nevtay <nevintay@gmail.com>
2020-06-03 22:13:27 +0800

    style: format with linting rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AddReportComponent } from './components/add-report/add-report.component';
-import { ViewReportsComponent } from './components/view-reports/view-reports.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ReportDetailComponent } from './components/report-detail/report-detail.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { AddReportComponent } from "./components/add-report/add-report.component";
+import { ViewReportsComponent } from "./components/view-reports/view-reports.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { ReportDetailComponent } from "./components/report-detail/report-detail.component";
 
 @NgModule({
   declarations: [
