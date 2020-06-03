0679672769fec908814140a73f2a3231b85e08a1
nevtay <nevintay@gmail.com>
2020-06-03 22:00:14 +0800

    style: format with linting rules; feat: add functionality to clear all reports from local storage; feat: add code to ngOnInit to generate one report for quick demonstration if there are no reports; refactor: remove unused 'REPORTS' import;

--- src/app/components/view-reports/view-reports.component.ts
+++ src/app/components/view-reports/view-reports.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Report } from '../../models/Report'
-import { REPORTS } from  '../../models/Reports'
+import { Component, OnInit, Input } from "@angular/core";
+import { Report } from "../../models/Report";
 
 @Component({
-  selector: 'app-view-reports',
-  templateUrl: './view-reports.component.html',
-  styleUrls: ['./view-reports.component.css']
+  selector: "app-view-reports",
+  templateUrl: "./view-reports.component.html",
+  styleUrls: ["./view-reports.component.css"]
 })
 export class ViewReportsComponent implements OnInit {
   @Input() report: Report;
-  
-  reports = REPORTS;
+  reports = JSON.parse(localStorage.getItem("reports"));
   selectedReport: Report;
 
-  constructor() { }
-  
-  ngOnInit() {
+  constructor () { }
+
+  ngOnInit () {
+    // produce one mock report if there no actual reports - for demonstration only
+    if (!JSON.parse(localStorage.getItem("reports"))) {
+      localStorage.setItem("reports", JSON.stringify([{ username: "sampleuser", date: "2020-05-30", content: "sample report", isApproved: "false" }]));
+    }
   }
 
-  onSelect(report: Report): void {
+  onSelect (report: Report): void {
     this.selectedReport = report;
   }
-  
+
+  clearAllReports () {
+    localStorage.clear();
+    window.location.reload();
+  }
 }
