709b353a8c675e9f3f4f5d346df36915c000b50f
nevtay <nevintay@gmail.com>
2020-06-03 22:13:57 +0800

    style: format with linting rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { AddReportComponent } from './components/add-report/add-report.component';
-import { ViewReportsComponent } from './components/view-reports/view-reports.component';
-import { RegisterComponent } from './components/register/register.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { AddReportComponent } from "./components/add-report/add-report.component";
+import { ViewReportsComponent } from "./components/view-reports/view-reports.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'create-report', component: AddReportComponent },
-  { path: 'view-reports', component: ViewReportsComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: "", component: LoginComponent },
+  { path: "create-report", component: AddReportComponent },
+  { path: "view-reports", component: ViewReportsComponent },
+  { path: "register", component: RegisterComponent }
 ];
 
 @NgModule({
