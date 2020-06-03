d270e2fbc2b8e0618c14c29643f92cb792dcc96d
nevtay <nevintay@gmail.com>
2020-06-03 22:03:55 +0800

    style: format with linting rules

--- src/app/components/report-detail/report-detail.component.ts
+++ src/app/components/report-detail/report-detail.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Report } from '../../models/Report'
+import { Component, OnInit, Input } from "@angular/core";
+import { Report } from "../../models/Report";
 
 @Component({
-  selector: 'app-report-detail',
-  templateUrl: './report-detail.component.html',
-  styleUrls: ['./report-detail.component.css']
+  selector: "app-report-detail",
+  templateUrl: "./report-detail.component.html",
+  styleUrls: ["./report-detail.component.css"]
 })
 export class ReportDetailComponent implements OnInit {
   isReadOnly:boolean = true;
 
   @Input() report: Report;
 
-  constructor() { }
+  constructor () { }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
-  toggleReportEditing() {
+  toggleReportEditing () {
     this.isReadOnly = !this.isReadOnly;
   }
-
 }
