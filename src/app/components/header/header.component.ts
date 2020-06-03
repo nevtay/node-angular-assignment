1685b2bcf8219defa7be04335d1b8f99d8db7d57
nevtay <nevintay@gmail.com>
2020-06-03 22:07:14 +0800

    style: format with linting rules; refactor: remove unused file imports

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  constructor () { }
 
-  constructor(
-    private route: ActivatedRoute
-  ) { }
-
-  ngOnInit() { }
-
+  ngOnInit () { }
 }
