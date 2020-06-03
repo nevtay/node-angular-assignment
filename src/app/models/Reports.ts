962c0858addfc6c66fba6afcf4e04f5a8a3f40ee
nevtay <nevintay@gmail.com>
2020-06-03 21:55:30 +0800

    style: format with linting rules

--- src/app/models/Reports.ts
+++ src/app/models/Reports.ts
@@ -1,8 +1,8 @@
-import { Report } from "./Report"
+import { Report } from "./Report";
 
 export const REPORTS: Report[] = [
-    { date: '13 May 2020', username: 'User 1', content: 'sample content', isApproved: false },
-    { date: '14 May 2020', username: 'User 2', content: 'sample content2', isApproved: false },
-    { date: '15 May 2020', username: 'User 3', content: 'sample content3', isApproved: false },
-    { date: '16 May 2020', username: 'User 4', content: 'sample content4', isApproved: false }
-]
+  { date: "13 May 2020", username: "User 1", content: "sample content", isApproved: false },
+  { date: "14 May 2020", username: "User 2", content: "sample content2", isApproved: false },
+  { date: "15 May 2020", username: "User 3", content: "sample content3", isApproved: false },
+  { date: "16 May 2020", username: "User 4", content: "sample content4", isApproved: false }
+];
