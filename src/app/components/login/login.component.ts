982f272134cb39f193100c7275c581222bd1fb98
nevtay <nevintay@gmail.com>
2020-06-03 22:06:41 +0800

    style: format with linting rules; refactor: remove unused file imports and the code that relied on them

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,39 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { LoginCredentials } from '../../models/LoginCredentials'
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { LoginCredentials } from "../../models/LoginCredentials";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  username:string = '';
-  password:string = '';
+  username:string = "";
+  password:string = "";
   loginModel = new LoginCredentials(this.username, this.password)
 
-  updateUsername(value: string) {
+  updateUsername (value: string) {
     this.username = value;
   }
 
-  updatePassword(value: string) {
+  updatePassword (value: string) {
     this.password = value;
   }
 
-  attemptLogin(f: NgForm) {
-    console.log(f.value)
+  attemptLogin (f: NgForm) {
+    console.log(f.value);
   }
 
-  constructor(
+  constructor (
     private route: ActivatedRoute
   ) { }
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      this.username = params['username'];
-      this.password = params['password'];
-    });
+  ngOnInit () {
   }
-
 }
