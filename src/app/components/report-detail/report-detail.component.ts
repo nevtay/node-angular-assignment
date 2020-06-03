79a36fa6d6644a76148fed3c29bc1ef49bbb3a13
nevtay <nevintay@gmail.com>
2020-06-03 17:22:00 +0800

    feat: initial commit

---
+++ src/app/components/report-detail/report-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Report } from '../../models/Report'
+
+@Component({
+  selector: 'app-report-detail',
+  templateUrl: './report-detail.component.html',
+  styleUrls: ['./report-detail.component.css']
+})
+export class ReportDetailComponent implements OnInit {
+  isReadOnly:boolean = true;
+
+  @Input() report: Report;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleReportEditing() {
+    this.isReadOnly = !this.isReadOnly;
+  }
+
+}
